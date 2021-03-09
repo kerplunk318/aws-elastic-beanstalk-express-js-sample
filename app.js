c8e77418581bc2969a53a8e84bae45b5529b0bdf
kerplunk318 <louis.gaginis@gmail.com>
2021-03-09 18:41:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+<<<<<<< HEAD
 app.get('/', (r, res) => res.se('Testing the build pipeline!'));
+=======
+app.get('/', (req, res) => res.send('Testing the build pipeline!'));
+>>>>>>> parent of 1f89cf3 (Update app.js)
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
